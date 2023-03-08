42e56ea5ab1b949eda99d8eb88b1debcd012e4a3
Marko Cuk <marko.cuk.brains22@gmail.com>
2023-03-08 13:53:11 +0100

    Prvi commit

---
+++ src/main/java/com/iktpreobuka/project/ZavrsniradApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZavrsniradApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZavrsniradApplication.class, args);
+	}
+
+}
